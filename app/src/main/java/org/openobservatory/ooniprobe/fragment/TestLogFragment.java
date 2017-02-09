54a4b031a8c04f577caced26e949d4078e795a29
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2017-02-09 01:00:17 +0100

    String file

--- app/src/main/java/org/openobservatory/ooniprobe/fragment/TestLogFragment.java
+++ app/src/main/java/org/openobservatory/ooniprobe/fragment/TestLogFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.openobservatory.ooniprobe.R;
