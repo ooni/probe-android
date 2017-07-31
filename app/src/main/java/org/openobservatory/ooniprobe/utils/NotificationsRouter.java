6bdd3145ad43c037c9c89c00ccdf91194a3d3101
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2017-07-31 17:34:51 +0200

    Handle notification json correctly

--- app/src/main/java/org/openobservatory/ooniprobe/utils/NotificationsRouter.java
+++ app/src/main/java/org/openobservatory/ooniprobe/utils/NotificationsRouter.java
@@ -4,6 +4,14 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+
 public class NotificationsRouter extends FirebaseMessagingService {
 
     @Override
@@ -12,12 +20,31 @@ public class NotificationsRouter extends FirebaseMessagingService {
 
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Message from: " + remoteMessage.getFrom());
+        Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        Map<String, String> params = remoteMessage.getData();
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-            // TODO
+            try {
+                JSONObject data = new JSONObject(params.toString());
+                if (data.getString("type").equals("open_href")){
+                    JSONObject payload = data.getJSONObject("payload");
+                    String href = payload.getString("href");
+                    JSONArray alt_hrefs = payload.getJSONArray("alt_hrefs");
+                    ArrayList<String>urls = new ArrayList<>();
+                    urls.add(href);
+                    for(int i=0; i< alt_hrefs.length(); i++)
+                    {
+                        urls.add(alt_hrefs.getString(i));
+                    }
+                    Log.d(TAG, "Message data urls: " + urls);
+                }
+            }
+            catch (Exception e) {
+                System.out.println("JSONException "+ e);
+            }
+
         }
 
         // Check if message contains a notification payload.
@@ -36,6 +63,18 @@ public class NotificationsRouter extends FirebaseMessagingService {
     @Override
     public void onDeletedMessages() {
         // TODO
+    }
+
+    private JSONObject getJsonFromMap(Map<String, Object> map) throws JSONException {
+        JSONObject jsonData = new JSONObject();
+        for (String key : map.keySet()) {
+            Object value = map.get(key);
+            if (value instanceof Map<?, ?>) {
+                value = getJsonFromMap((Map<String, Object>) value);
+            }
+            jsonData.put(key, value);
+        }
+        return jsonData;
     }
 
     private final String TAG = "NotificationsRouter";
