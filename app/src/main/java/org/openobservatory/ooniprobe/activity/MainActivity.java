ad22b3709a3e1df37ff718ad1fdf98e748ab2592
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2017-09-24 17:57:41 +0200

    Removed RELEASE_NAME

--- app/src/main/java/org/openobservatory/ooniprobe/activity/MainActivity.java
+++ app/src/main/java/org/openobservatory/ooniprobe/activity/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity  implements Observer {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.activity_main);
         checkResources();
 
