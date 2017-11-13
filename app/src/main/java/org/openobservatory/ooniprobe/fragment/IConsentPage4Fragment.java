62022d54594c3a40533f9c09b63035abb4af2a2e
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2017-11-13 16:44:48 +0100

    Implemented Lottie

--- app/src/main/java/org/openobservatory/ooniprobe/fragment/IConsentPage4Fragment.java
+++ app/src/main/java/org/openobservatory/ooniprobe/fragment/IConsentPage4Fragment.java
@@ -1,9 +1,9 @@
 package org.openobservatory.ooniprobe.fragment;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
@@ -12,15 +12,10 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.gif.GifDrawable;
-import com.bumptech.glide.request.target.DrawableImageViewTarget;
-import com.bumptech.glide.request.transition.Transition;
+import com.airbnb.lottie.LottieAnimationView;
 
 import org.openobservatory.ooniprobe.R;
 import org.openobservatory.ooniprobe.activity.InformedConsentActivity;
-
-import android.os.Handler;
 
 public class IConsentPage4Fragment extends Fragment {
 
@@ -29,7 +24,7 @@ public class IConsentPage4Fragment extends Fragment {
     private AppCompatButton falseButton;
     private TextView questionNumber;
     private TextView questionText;
-    private ImageView gifView;
+    private LottieAnimationView animationView;
 
     public static IConsentPage4Fragment create() {
         IConsentPage4Fragment atf = new IConsentPage4Fragment();
@@ -66,8 +61,8 @@ public class IConsentPage4Fragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_ic_page_4, container, false);
         questionNumber = v.findViewById(R.id.question_number);
         questionText = v.findViewById(R.id.question_text);
-        gifView = v.findViewById(R.id.gifView);
-        gifView.setVisibility(View.GONE);
+        animationView = v.findViewById(R.id.animationView);
+        //animationView.setVisibility(View.GONE);
         loadView();
         trueButton = v.findViewById(R.id.trueButton);
         trueButton.setOnClickListener(new View.OnClickListener() {
@@ -94,32 +89,21 @@ public class IConsentPage4Fragment extends Fragment {
     }
 
     private void next(final Boolean answer){
-        /*
-                Glide is a bit slow, alternatives
-                https://stackoverflow.com/questions/29363321/picasso-v-s-imageloader-v-s-fresco-vs-glide
-                https://github.com/Cutta/GifView/
-                https://github.com/koral--/android-gif-drawable
-                 */
-                /*
-                Can't know when gif ends in glide 4, have to set timer
-                https://github.com/bumptech/glide/issues/860
-                https://github.com/bumptech/glide/issues/2524
-                ALT: http://frescolib.org/docs/animations.html#playing-animations-manually
-                 */
+        animationView.setImageAssetsFolder("anim/");
+        animationView.setAnimation("anim/tickmark.json");
+        animationView.loop(false);
+        animationView.playAnimation();
+        animationView.addAnimatorListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animationView.cancelAnimation();
+                animationView.setProgress(0);
+            }
+        });
+        //animationView.animate();
 
-        gifView.setVisibility(View.VISIBLE);
-        Glide.with(mActivity)
-                .load(answer? R.drawable.correct_answer : R.drawable.correct_answer)
-                .into(new DrawableImageViewTarget(gifView) {
-                    @Override
-                    public void onResourceReady(Drawable resource, @Nullable Transition<? super Drawable> transition) {
-                        if (resource instanceof GifDrawable) {
-                            ((GifDrawable)resource).setLoopCount(1);
-                            Handler handler = new Handler();
-                            handler.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    gifView.setVisibility(View.GONE);
+        //                .load(answer? R.drawable.correct_answer : R.drawable.correct_answer)
+
                                     /*if (!answer)
                                     show popup actually
                                     else if (mActivity.QUESTION_NUMBER == 1) {
@@ -128,15 +112,9 @@ public class IConsentPage4Fragment extends Fragment {
                                      else if (mActivity.QUESTION_NUMBER == 2) {
                                      go next
                                      */
-                                    if (mActivity.QUESTION_NUMBER == 1) {
-                                        mActivity.QUESTION_NUMBER = 2;
-                                        loadView();
-                                    }
-                                }
-                            }, 3000);
-                        }
-                        super.onResourceReady(resource, transition);
-                    }
-                });
+        /*if (mActivity.QUESTION_NUMBER == 1) {
+            mActivity.QUESTION_NUMBER = 2;
+            loadView();
+        }*/
     }
 }
