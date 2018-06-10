0d2e08af77f674094734ceff7ef4e84b2bdff888
Alessandro Scarozza <xan.scale@gmail.com>
2018-06-10 16:57:22 +0200

    copyResources

--- app/src/main/java/org/openobservatory/ooniprobe/common/Application.java
+++ app/src/main/java/org/openobservatory/ooniprobe/common/Application.java
@@ -2,7 +2,13 @@ package org.openobservatory.ooniprobe.common;
 
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
+import com.google.android.gms.common.util.IOUtils;
 import com.google.firebase.FirebaseApp;
+
+import org.openobservatory.ooniprobe.R;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -19,8 +25,21 @@ public class Application extends android.app.Application {
 		CrashlyticsCore core = new CrashlyticsCore.Builder().disabled(preferenceManager.isSendCrash()).build();
 		Fabric.with(this, new Crashlytics.Builder().core(core).build());
 		FirebaseApp.initializeApp(this);
+		copyResources(R.raw.geoipasnum, "GeoIPASNum.dat");
+		copyResources(R.raw.geoip, "GeoIP.dat");
 	}
 
-
-
+	private void copyResources(int id, String filename) {
+		try {
+			openFileInput(filename).close();
+		} catch (FileNotFoundException e) {
+			try {
+				IOUtils.copyStream(getResources().openRawResource(id), openFileOutput(filename, MODE_PRIVATE), true);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
