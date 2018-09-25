c466890abb6ba99e5624d7778997137e4a70b1e6
Alessandro Scarozza <xan.scale@gmail.com>
2018-09-25 22:14:13 +0200

    clean json urls

--- app/src/main/java/org/openobservatory/ooniprobe/activity/RunningActivity.java
+++ app/src/main/java/org/openobservatory/ooniprobe/activity/RunningActivity.java
@@ -11,13 +11,25 @@ import android.widget.TextView;
 import com.airbnb.lottie.LottieAnimationView;
 
 import org.openobservatory.ooniprobe.R;
+import org.openobservatory.ooniprobe.common.OoniIOClient;
+import org.openobservatory.ooniprobe.model.RetrieveUrlResponse;
 import org.openobservatory.ooniprobe.model.database.Result;
+import org.openobservatory.ooniprobe.model.database.Url;
 import org.openobservatory.ooniprobe.test.TestAsyncTask;
 import org.openobservatory.ooniprobe.test.suite.AbstractSuite;
 import org.openobservatory.ooniprobe.test.test.AbstractTest;
+import org.openobservatory.ooniprobe.test.test.WebConnectivity;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import localhost.toolkit.app.MessageDialogFragment;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RunningActivity extends AbstractActivity {
 	public static final String TEST = "test";
@@ -25,6 +37,7 @@ public class RunningActivity extends AbstractActivity {
 	@BindView(R.id.log) TextView log;
 	@BindView(R.id.progress) ProgressBar progress;
 	@BindView(R.id.animation) LottieAnimationView animation;
+	private AbstractSuite testSuite;
 
 	public static Intent newIntent(Context context, AbstractSuite testSuite) {
 		return new Intent(context, RunningActivity.class).putExtra(TEST, testSuite);
@@ -32,7 +45,7 @@ public class RunningActivity extends AbstractActivity {
 
 	@Override protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AbstractSuite testSuite = (AbstractSuite) getIntent().getSerializableExtra(TEST);
+		testSuite = (AbstractSuite) getIntent().getSerializableExtra(TEST);
 		setTheme(testSuite.getThemeDark());
 		setContentView(R.layout.activity_running);
 		ButterKnife.bind(this);
@@ -43,12 +56,46 @@ public class RunningActivity extends AbstractActivity {
 		AbstractTest[] testList = testSuite.getTestList(getPreferenceManager());
 		if (testList != null) {
 			progress.setMax(testList.length * 100);
-			new TestAsyncTaskImpl(this, testSuite.getName()).execute(testList);
+			boolean isWebConn = false;
+			for (AbstractTest abstractTest : testList)
+				if (abstractTest instanceof WebConnectivity) {
+					isWebConn = true;
+					break;
+				}
+			if (isWebConn) {
+				Retrofit retrofit = new Retrofit.Builder().baseUrl("https://events.proteus.test.ooni.io/").addConverterFactory(GsonConverterFactory.create()).build();
+				retrofit.create(OoniIOClient.class).getUrls("IT", getPreferenceManager().isAllCategoryEnabled() ? null : getPreferenceManager().getEnabledCategory()).enqueue(new Callback<RetrieveUrlResponse>() {
+					@Override public void onResponse(Call<RetrieveUrlResponse> call, Response<RetrieveUrlResponse> response) {
+						if (response.isSuccessful() && response.body() != null && response.body().results != null) {
+							ArrayList<String> inputs = new ArrayList<>();
+							for (Url url : response.body().results) {
+								Url storedUrl = Url.checkExistingUrl(url.url, url.category_code, url.country_code);
+								inputs.add(storedUrl.url);
+							}
+							for (AbstractTest abstractTest : testList)
+								if (abstractTest instanceof WebConnectivity) {
+									WebConnectivity wc = (WebConnectivity) abstractTest;
+									wc.setInputs(inputs);
+								}
+							run(testList);
+						}
+					}
+
+					@Override public void onFailure(Call<RetrieveUrlResponse> call, Throwable t) {
+						MessageDialogFragment.newInstance(getString(R.string.Modal_Error), getString(R.string.Modal_Error_CantDownloadUrls), true).show(getFragmentManager(), null);
+					}
+				});
+			} else
+				run(testList);
 		}
 	}
 
 	@Override public void onBackPressed() {
 		// TODO add toast to
+	}
+
+	private void run(AbstractTest[] testList) {
+		new TestAsyncTaskImpl(this, testSuite.getName()).execute(testList);
 	}
 
 	private static class TestAsyncTaskImpl extends TestAsyncTask<RunningActivity> {
--- app/src/main/java/org/openobservatory/ooniprobe/test/test/WebConnectivity.java
+++ app/src/main/java/org/openobservatory/ooniprobe/test/test/WebConnectivity.java
@@ -7,25 +7,18 @@ import com.google.gson.Gson;
 
 import org.openobservatory.ooniprobe.R;
 import org.openobservatory.ooniprobe.common.PreferenceManager;
-import org.openobservatory.ooniprobe.common.OoniIOClient;
-import org.openobservatory.ooniprobe.model.RetrieveUrlResponse;
 import org.openobservatory.ooniprobe.model.database.Measurement;
 import org.openobservatory.ooniprobe.model.database.Result;
-import org.openobservatory.ooniprobe.model.database.Url;
 import org.openobservatory.ooniprobe.model.jsonresult.JsonResult;
 import org.openobservatory.ooniprobe.model.settings.Settings;
 
-import java.io.IOException;
 import java.util.ArrayList;
-
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class WebConnectivity extends AbstractTest {
 	public static final String NAME = "web_connectivity";
 	public static final String MK_NAME = "WebConnectivity";
 	private ArrayList<String> inputs;
+	private Float max_runtime;
 
 	public WebConnectivity() {
 		super(NAME, MK_NAME, R.string.Test_WebConnectivity_Fullname, 0);
@@ -33,27 +26,17 @@ public class WebConnectivity extends AbstractTest {
 
 	@Override public void run(Context c, PreferenceManager pm, Gson gson, Result result, int index, TestCallback testCallback) {
 		Settings settings = new Settings(c, pm);
-		if (inputs == null) {
-			settings.options.max_runtime = pm.getMaxRuntime();
-			settings.inputs = new ArrayList<>();
-			try {
-				Retrofit retrofit = new Retrofit.Builder().baseUrl("https://events.proteus.test.ooni.io/").addConverterFactory(GsonConverterFactory.create()).build();
-				Response<RetrieveUrlResponse> response = retrofit.create(OoniIOClient.class).getUrls("IT", pm.isAllCategoryEnabled() ? null : pm.getEnabledCategory()).execute();
-				if (response.isSuccessful() && response.body() != null && response.body().results != null)
-					for (Url url : response.body().results) {
-						Url storedUrl = Url.checkExistingUrl(url.url, url.category_code, url.country_code);
-						settings.inputs.add(storedUrl.url);
-					}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else
-			settings.inputs = inputs;
+		settings.options.max_runtime = max_runtime == null ? pm.getMaxRuntime() : max_runtime;
+		settings.inputs = inputs;
 		run(c, pm, gson, settings, result, index, testCallback);
 	}
 
 	public void setInputs(ArrayList<String> inputs) {
 		this.inputs = inputs;
+	}
+
+	public void setMax_runtime(Float max_runtime) {
+		this.max_runtime = max_runtime;
 	}
 
 	@Override public void onEntry(Context c, PreferenceManager pm, @NonNull JsonResult json, Measurement measurement) {
