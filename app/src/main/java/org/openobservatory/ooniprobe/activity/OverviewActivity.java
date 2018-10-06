23fe3f2ac37471945bb381cd600e6d4fa5ddc18b
Alessandro Scarozza <xan.scale@gmail.com>
2018-10-06 11:16:51 +0200

    replaced markdown

--- app/src/main/java/org/openobservatory/ooniprobe/activity/OverviewActivity.java
+++ app/src/main/java/org/openobservatory/ooniprobe/activity/OverviewActivity.java
@@ -3,8 +3,6 @@ package org.openobservatory.ooniprobe.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,12 +10,12 @@ import android.widget.TextView;
 import org.openobservatory.ooniprobe.R;
 import org.openobservatory.ooniprobe.test.suite.AbstractSuite;
 
-import br.tiagohm.markdownview.MarkdownView;
-import br.tiagohm.markdownview.css.InternalStyleSheet;
-import br.tiagohm.markdownview.css.styles.Github;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import ru.noties.markwon.Markwon;
 
 public class OverviewActivity extends AbstractActivity {
 	public static final String TEST = "test";
@@ -25,7 +23,7 @@ public class OverviewActivity extends AbstractActivity {
 	@BindView(R.id.icon) ImageView icon;
 	@BindView(R.id.title) TextView title;
 	@BindView(R.id.configure) Button configure;
-	@BindView(R.id.desc) MarkdownView desc;
+	@BindView(R.id.desc) TextView desc;
 	private AbstractSuite testSuite;
 
 	public static Intent newIntent(Context context, AbstractSuite testSuite) {
@@ -35,7 +33,7 @@ public class OverviewActivity extends AbstractActivity {
 	@Override protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		testSuite = (AbstractSuite) getIntent().getSerializableExtra(TEST);
-		setTheme(testSuite.getThemeDark());
+		setTheme(testSuite.getThemeLight());
 		setContentView(R.layout.activity_overview);
 		ButterKnife.bind(this);
 		setSupportActionBar(toolbar);
@@ -43,11 +41,7 @@ public class OverviewActivity extends AbstractActivity {
 		getSupportActionBar().setDisplayShowTitleEnabled(false);
 		icon.setImageResource(testSuite.getIcon());
 		title.setText(testSuite.getTitle());
-		InternalStyleSheet css = new Github();
-		// TODO try https://github.com/noties/Markwon
-		css.addFontFace("Fira Sans", null, null, null, "url('https://fonts.googleapis.com/css?family=Fira+Sans')");
-		desc.addStyleSheet(css);
-		desc.loadMarkdown(getString(testSuite.getDesc1()) + "\n\n" + getString(testSuite.getDesc2()));
+		Markwon.setMarkdown(desc, getString(testSuite.getDesc1()) + "\n\n" + getString(testSuite.getDesc2()));
 	}
 
 	@OnClick(R.id.configure) void onConfigureClick() {
