489992bee9c3a7488d2f7e912c104d702a1967ef
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2018-12-21 15:04:22 +0700

    removed unused scripts

--- scripts/update-geoip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-base=https://download.maxmind.com/download/geoip/database
-wget $base/GeoLiteCountry/GeoIP.dat.gz -O GeoIP.dat.gz
-gzip -d GeoIP.dat.gz
-wget $base/asnum/GeoIPASNum.dat.gz -O GeoIPASNum.dat.gz
-gzip -d GeoIPASNum.dat.gz
-mv GeoIP.dat ../app/src/main/res/raw/geoip.dat
-mv GeoIPASNum.dat ../app/src/main/res/raw/geoipasnum.dat
--- scripts/update_languages.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -u
-tx pull -f
-tx pull -f -l zh_CN
-tx pull -f -l zh_TW
-cp app/src/main/res/values-zh_CN/strings.xml app/src/main/res/values-zh-rCN/strings.xml 
-cp app/src/main/res/values-zh_TW/strings.xml app/src/main/res/values-zh-rTW/strings.xml 
-rm -Rf app/src/main/res/values-zh_CN app/src/main/res/values-zh_TW
-python scripts/fix-strings.py
