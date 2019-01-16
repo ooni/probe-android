b678fcea4c16dd5e83fd7a32eff67fbf884c50fa
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-01-16 10:17:30 +0700

    clean old comment

--- app/src/main/java/org/openobservatory/ooniprobe/utils/VersionUtils.java
+++ app/src/main/java/org/openobservatory/ooniprobe/utils/VersionUtils.java
@@ -5,8 +5,6 @@ import org.openobservatory.ooniprobe.BuildConfig;
 public class VersionUtils {
 
     public static String get_software_version(){
-        //FIXME: submitting the VERSION_CODE breaks OONI backend
-        //return BuildConfig.VERSION_NAME + "+" + BuildConfig.VERSION_CODE;
         return BuildConfig.VERSION_NAME;
     }
 }
