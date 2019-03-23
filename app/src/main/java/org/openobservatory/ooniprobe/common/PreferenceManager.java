c35e74584f709a23f9edc16cc8e07fbdfd2a08bf
Alessandro Scarozza <xan.scale@gmail.com>
2019-03-23 13:35:43 +0100

    add Manual Upload Dialog

--- app/src/main/java/org/openobservatory/ooniprobe/common/PreferenceManager.java
+++ app/src/main/java/org/openobservatory/ooniprobe/common/PreferenceManager.java
@@ -10,7 +10,8 @@ import org.openobservatory.ooniprobe.R;
 import java.util.ArrayList;
 
 public class PreferenceManager {
-	public static final String GEO_VER = "geo_ver";
+	static final String GEO_VER = "geo_ver";
+	private static final String IS_MANUAL_UPLOAD_DIALOG = "isManualUploadDialog";
 	private static final String TOKEN = "token";
 	private static final String SHOW_ONBOARDING = "first_run";
 	private final SharedPreferences sp;
@@ -59,7 +60,11 @@ public class PreferenceManager {
 	}
 
 	public void setShowOnboarding(boolean showIntro) {
-		sp.edit().putBoolean(SHOW_ONBOARDING, showIntro).apply();
+		sp.edit().putBoolean(SHOW_ONBOARDING, showIntro).putBoolean(IS_MANUAL_UPLOAD_DIALOG, showIntro).apply();
+	}
+
+	public boolean isManualUploadDialog() {
+		return sp.getBoolean(IS_MANUAL_UPLOAD_DIALOG, true);
 	}
 
 	private boolean isNotifications() {
@@ -80,6 +85,10 @@ public class PreferenceManager {
 
 	public boolean isManualUploadResults() {
 		return sp.getBoolean(r.getString(R.string.upload_results_manual), true);
+	}
+
+	public void setManualUploadResults(boolean manualUpload) {
+		sp.edit().putBoolean(r.getString(R.string.upload_results_manual), manualUpload).apply();
 	}
 
 	public boolean isIncludeIp() {
