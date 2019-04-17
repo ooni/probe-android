5767878139abaf1f10263e71bd7ff65e02d54f30
Alessandro Scarozza <xan.scale@gmail.com>
2019-04-17 21:26:40 +0200

    convert DateJsonDeserializer to full DateAdapter (#216)
    * convert DateJsonDeserializer to full DateAdapter (serialization/deserialization) with testcase
    * use LocalhostToolkit as public release
    *

--- app/src/androidTest/java/org/openobservatory/ooniprobe/Suite.java
+++ app/src/androidTest/java/org/openobservatory/ooniprobe/Suite.java
@@ -2,6 +2,7 @@ package org.openobservatory.ooniprobe;
 
 import org.junit.runner.RunWith;
 import org.openobservatory.ooniprobe.common.ApplicationTest;
+import org.openobservatory.ooniprobe.common.DateAdapterTest;
 import org.openobservatory.ooniprobe.model.database.NetworkTest;
 import org.openobservatory.ooniprobe.model.jsonresult.TestKeysTest;
 
@@ -11,6 +12,7 @@ import org.openobservatory.ooniprobe.model.jsonresult.TestKeysTest;
 @RunWith(org.junit.runners.Suite.class)
 @org.junit.runners.Suite.SuiteClasses({
 		ApplicationTest.class,
+		DateAdapterTest.class,
 		NetworkTest.class,
 		TestKeysTest.class
 })
