df1b8081681cc99319fa76a0670112d775a2d788
Alessandro Scarozza <xan.scale@gmail.com>
2019-04-17 13:11:52 +0200

    fix rename

--- app/src/full/java/org/openobservatory/ooniprobe/common/FirebaseMessagingService.java
+++ app/src/full/java/org/openobservatory/ooniprobe/common/FirebaseMessagingService.java
@@ -40,6 +40,6 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
 	@Override public void onNewToken(String token) {
 		((Application) getApplicationContext()).getPreferenceManager().setToken(token);
-		MKOrchestraClient.sync((Application) getApplicationContext());
+		MKOrchestraSettings.sync((Application) getApplicationContext());
 	}
 }
