95e4b7e4f1d3ed5bd6ecb845bf3e8d68b92b4aea
Alessandro Scarozza <xan.scale@gmail.com>
2019-04-17 21:04:56 +0200

    add DateAdapterTest

--- app/src/androidTest/java/org/openobservatory/ooniprobe/AbstractTest.java
+++ app/src/androidTest/java/org/openobservatory/ooniprobe/AbstractTest.java
@@ -3,13 +3,16 @@ package org.openobservatory.ooniprobe;
 import android.content.Context;
 
 import org.junit.Before;
+import org.openobservatory.ooniprobe.common.Application;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 
 public class AbstractTest {
 	protected Context c;
+	protected Application a;
 
 	@Before public void before() {
 		c = InstrumentationRegistry.getInstrumentation().getTargetContext();
+		a = (Application) c.getApplicationContext();
 	}
 }
