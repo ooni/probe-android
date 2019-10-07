fe12d62d3bec2dc3640c66d66701c093a17e7b33
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-10-07 10:50:35 +0800

    Add react-native

--- app/src/main/java/org/openobservatory/ooniprobe/activity/InfoActivity.java
+++ app/src/main/java/org/openobservatory/ooniprobe/activity/InfoActivity.java
@@ -1,38 +1,100 @@
 package org.openobservatory.ooniprobe.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
-import android.widget.TextView;
+import android.provider.Settings;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import org.openobservatory.ooniprobe.BuildConfig;
-import org.openobservatory.ooniprobe.R;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import io.ooni.mk.MKVersion;
+public class InfoActivity extends Activity implements DefaultHardwareBackBtnHandler {
+	private ReactRootView mReactRootView;
+	private ReactInstanceManager mReactInstanceManager;
+	private final int OVERLAY_PERMISSION_REQ_CODE = 1;
 
-public class InfoActivity extends AbstractActivity {
-	@BindView(R.id.toolbar) Toolbar toolbar;
-	@BindView(R.id.version) TextView version;
-
-	@Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_info);
-		ButterKnife.bind(this);
-		setSupportActionBar(toolbar);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		version.setText(getString(R.string.version, BuildConfig.VERSION_NAME, MKVersion.getVersionMK()));
+		SoLoader.init(this, false);
+		mReactRootView = new ReactRootView(this);
+		mReactInstanceManager = ReactInstanceManager.builder()
+				.setApplication(getApplication())
+				.setCurrentActivity(this)
+				.setBundleAssetName("index.android.bundle")
+				.setJSMainModulePath("index")
+				.addPackage(new MainReactPackage())
+				.setUseDeveloperSupport(BuildConfig.DEBUG)
+				.setInitialLifecycleState(LifecycleState.RESUMED)
+				.build();
+		// The string here (e.g. "MyReactNativeApp") has to match
+		// the string in AppRegistry.registerComponent() in index.js
+		mReactRootView.startReactApplication(mReactInstanceManager, "HelloWorld", null);
+
+		setContentView(mReactRootView);
+
 	}
 
-	@OnClick(R.id.learnMore) void onLearnMoreClick() {
-		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://ooni.io/")));
+	@Override
+	public void invokeDefaultOnBackPressed() {
+		super.onBackPressed();
 	}
 
-	@OnClick(R.id.dataPolicy) void onDataPolicyClick() {
-		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://ooni.io/about/data-policy/")));
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		if (mReactInstanceManager != null) {
+			mReactInstanceManager.onHostPause(this);
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		if (mReactInstanceManager != null) {
+			mReactInstanceManager.onHostResume(this, this);
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		if (mReactInstanceManager != null) {
+			mReactInstanceManager.onHostDestroy(this);
+		}
+		if (mReactRootView != null) {
+			mReactRootView.unmountReactApplication();
+		}
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (mReactInstanceManager != null) {
+			mReactInstanceManager.onBackPressed();
+		} else {
+			super.onBackPressed();
+		}
+	}
+
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+			mReactInstanceManager.showDevOptionsDialog();
+			return true;
+		}
+		return super.onKeyUp(keyCode, event);
 	}
 }
---
+++ app/src/main/java/org/openobservatory/ooniprobe/activity/InfoActivity_old.java
@@ -0,0 +1,38 @@
+package org.openobservatory.ooniprobe.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import android.widget.TextView;
+
+import org.openobservatory.ooniprobe.BuildConfig;
+import org.openobservatory.ooniprobe.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.ooni.mk.MKVersion;
+
+public class InfoActivity_old extends AbstractActivity {
+	@BindView(R.id.toolbar) Toolbar toolbar;
+	@BindView(R.id.version) TextView version;
+
+	@Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_info);
+		ButterKnife.bind(this);
+		setSupportActionBar(toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		version.setText(getString(R.string.version, BuildConfig.VERSION_NAME, MKVersion.getVersionMK()));
+	}
+
+	@OnClick(R.id.learnMore) void onLearnMoreClick() {
+		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://ooni.io/")));
+	}
+
+	@OnClick(R.id.dataPolicy) void onDataPolicyClick() {
+		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://ooni.io/about/data-policy/")));
+	}
+}
