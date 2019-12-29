d4d0449d29a02b79203b49e0892564284374f9eb
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-12-29 15:14:42 +0100

    Added progress bar

--- app/src/main/java/org/openobservatory/ooniprobe/activity/TextActivity.java
+++ app/src/main/java/org/openobservatory/ooniprobe/activity/TextActivity.java
@@ -4,11 +4,17 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
-import android.widget.TextView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
@@ -34,11 +40,13 @@ import okhttp3.Request;
 
 public class TextActivity extends AbstractActivity {
 	private Measurement measurement;
+	private String text;
 	private static final int TYPE_LOG = 1;
 	private static final int TYPE_JSON = 2;
 	private static final String TEST = "test";
 	private static final String TYPE = "type";
 	@BindView(R.id.webView) WebView webView;
+	@BindView(R.id.progressBar) ProgressBar progressBar;
 
 	public static Intent newIntent(Context context, int type, Measurement measurement) {
 		return new Intent(context, TextActivity.class).putExtra(TYPE, type).putExtra(TEST, measurement);
@@ -49,7 +57,32 @@ public class TextActivity extends AbstractActivity {
 		setContentView(R.layout.text);
 		ButterKnife.bind(this);
 		measurement = (Measurement) getIntent().getSerializableExtra(TEST);
-		new Thread(() -> runOnUiThread(() -> showText())).start();
+		webView.setWebChromeClient(new WebChromeClient());
+		setProgressBarVisibility(View.VISIBLE);
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				super.onPageStarted(view, url, favicon);
+				setProgressBarVisibility(View.VISIBLE);
+			}
+
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				super.onPageFinished(view, url);
+				setProgressBarVisibility(View.GONE);
+			}
+
+			@Override
+			public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+				super.onReceivedError(view, request, error);
+				new MessageDialogFragment.Builder()
+						.withTitle(getString(R.string.Modal_Error))
+						.withMessage(error.toString())
+						.build().show(getSupportFragmentManager(), null);
+				setProgressBarVisibility(View.GONE);
+			}
+		});
+		showText();
 	}
 
 	@Override public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,7 +93,7 @@ public class TextActivity extends AbstractActivity {
 	@Override public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.clipboard:
-				//((ClipboardManager) getSystemService(CLIPBOARD_SERVICE)).setPrimaryClip(ClipData.newPlainText(getString(R.string.General_AppName), textView.getText().toString()));
+				((ClipboardManager) getSystemService(CLIPBOARD_SERVICE)).setPrimaryClip(ClipData.newPlainText(getString(R.string.General_AppName), text));
 				Toast.makeText(this, R.string.Toast_CopiedToClipboard, Toast.LENGTH_SHORT).show();
 				return true;
 			default:
@@ -70,13 +103,15 @@ public class TextActivity extends AbstractActivity {
 
 	public void showText(){
 		switch (getIntent().getIntExtra(TYPE, 0)) {
-			case TYPE_LOG:
+			case TYPE_JSON:
 				//Try to open file, if it doesn't exist dont show Error dialog immediately but try to download the json from internet
 				try {
 					File entryFile = Measurement.getEntryFile(this, measurement.id, measurement.test_name);
 					String json = FileUtils.readFileToString(entryFile, Charset.forName("UTF-8"));
 					json = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create().toJson(new JsonParser().parse(json));
-					webView.loadData(json, "text/html; charset=utf-8", "utf-8");
+					text = json;
+					webView.loadDataWithBaseURL(null, json, "text/plain", "UTF-8", null);
+					//webView.loadData(json, "text/plain", "UTF-8");
 				} catch (Exception e) {
 					e.printStackTrace();
 					if (ReachabilityManager.getNetworkType(this).equals(ReachabilityManager.NO_INTERNET)) {
@@ -88,11 +123,12 @@ public class TextActivity extends AbstractActivity {
 					getApiClient().getMeasurement(measurement.report_id, null).enqueue(new GetMeasurementsCallback() {
 						@Override
 						public void onSuccess(ApiMeasurement.Result result) {
-							//TODO load measurement_url into WebView from the internet
+							//Download measurement data locally and displaying into the WebView
 							getOkHttpClient().newCall(new Request.Builder().url(result.measurement_url).build()).enqueue(new GetMeasurementJsonCallback() {
 								@Override
 								public void onSuccess(String json) {
-									webView.loadData(json, "text/html; charset=utf-8", "utf-8");
+									text = json;
+									webView.loadData(json, "text/plain", "UTF-8");
 								}
 								@Override
 								public void onError(String msg) {
@@ -112,17 +148,28 @@ public class TextActivity extends AbstractActivity {
 						}
 					});
 				}
-			case TYPE_JSON:
+				break;
+			case TYPE_LOG:
 				try {
 					File logFile = Measurement.getLogFile(this, measurement.result.id, measurement.test_name);
 					String log = FileUtils.readFileToString(logFile, Charset.forName("UTF-8"));
-					webView.loadData(log, "text/html; charset=utf-8", "utf-8");
+					text = log;
+					webView.loadDataWithBaseURL(null, log, "text/plain", "UTF-8", null);
 				} catch (Exception e) {
 					e.printStackTrace();
 					new MessageDialogFragment.Builder()
 							.withTitle(getString(R.string.Modal_Error_LogNotFound))
 							.build().show(getSupportFragmentManager(), null);
 				}
+				break;
 		}
 	}
+
+	private void setProgressBarVisibility(int visibility) {
+		// If a user returns back, a NPE may occur if WebView is still loading a page and then tries to hide a ProgressBar.
+		if (progressBar != null) {
+			progressBar.setVisibility(visibility);
+		}
+	}
+
 }
