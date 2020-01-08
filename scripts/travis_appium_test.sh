42e81029ae03d4d6539332db3dbe4c161ecc4f1a
Arturo Filastò <arturo@filasto.net>
2020-01-08 14:56:43 +0200

    Add gradle wrapper

--- scripts/travis_appium_test.sh
+++ scripts/travis_appium_test.sh
@@ -6,6 +6,6 @@ git clone https://github.com/ooni/probe-mobile-appium.git
 ./gradlew assembleDebug
 adb devices
 appium-doctor --android
-cd probe-mobile-appium && gradle androidRegression
+cd probe-mobile-appium && ../gradlew androidRegression
 # cat $HOME/appium_log.txt
 cat /home/travis/build/ooni/probe-android/probe-mobile-appium/build/reports/tests/androidRegression/classes/probe.mobile.appium.runners.LandingPageRunner.html
