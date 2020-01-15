4b83adaea93b5d4160c1d1b06e536d3b9a1bc560
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-01-15 10:03:42 +0700

    Updating report ids to fix travis

--- app/src/androidTest/java/org/openobservatory/ooniprobe/client/OONIAPIClientTest.java
+++ app/src/androidTest/java/org/openobservatory/ooniprobe/client/OONIAPIClientTest.java
@@ -21,8 +21,8 @@ import java.util.concurrent.TimeUnit;
 import okhttp3.Request;
 
 public class OONIAPIClientTest extends AbstractTest {
-    private static final String EXISTING_REPORT_ID = "20190113T202156Z_AS327931_CgoC3KbgM6zKajvIIt1AxxybJ1HbjwwWJjsJnlxy9rpcGY54VH";
-    private static final String EXISTING_REPORT_ID_2 = "20190702T000027Z_AS5413_6FT78sjp5qnESDVWlFlm6bfxxwOEqR08ySAwigTF6C8PFCbMsM";
+    private static final String EXISTING_REPORT_ID = "20200113T232417Z_AS577_JZoBZCfObPmU7bxkEKUImuWl40VEb00Q8ZYcifQj3MgchgjOVd";
+    private static final String EXISTING_REPORT_ID_2 = "20200113T235535Z_AS39891_ZsM2hkmJREbadpVf0dKARRLZFsrnX2LYI9PGi7HlyXFBwRyQGP";
     private static final String NONEXISTING_REPORT_ID = "EMPTY";
     private static final String NON_EXISTING_MEASUREMENT_URL = "https://api.ooni.io/api/v1/measurement/nonexistent-measurement-url";
     private static final String JSON_URL = "https://api.ooni.io/api/v1/measurement/temp-id-263478291";
