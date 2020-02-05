2447561c4e338250f63b1d6b2e5463cc8b5788a4
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-02-05 19:01:16 +0800

    Removed unused code

--- app/src/main/java/org/openobservatory/ooniprobe/common/AppDatabase.java
+++ app/src/main/java/org/openobservatory/ooniprobe/common/AppDatabase.java
@@ -5,10 +5,7 @@ import android.util.Log;
 import com.raizlabs.android.dbflow.annotation.Database;
 import com.raizlabs.android.dbflow.annotation.Migration;
 import com.raizlabs.android.dbflow.sql.SQLiteType;
-import com.raizlabs.android.dbflow.sql.language.SQLite;
 import com.raizlabs.android.dbflow.sql.migration.AlterTableMigration;
-import com.raizlabs.android.dbflow.sql.migration.BaseMigration;
-import com.raizlabs.android.dbflow.structure.database.DatabaseWrapper;
 
 import org.openobservatory.ooniprobe.model.database.Result;
 
@@ -19,11 +16,6 @@ public class AppDatabase {
 
     @Migration(version = 2, database = AppDatabase.class)
     public static class Migration2 extends AlterTableMigration<Result> {
-
-        public Migration2() {
-            super(Result.class);
-            Log.v("Creation", "Creation()");
-        }
 
         public Migration2(Class<Result> table) {
             super(table);
@@ -36,15 +28,6 @@ public class AppDatabase {
             addColumn(SQLiteType.TEXT, "failure_msg");
         }
 
-        /*
-        @Override
-        public void migrate(DatabaseWrapper database) {
-           /* SQLite.update(Result.class)
-                    .set(Result_table.status.eq("Invalid"))
-                    .where(Employee_Table.job.eq("Laid Off"))
-                    .execute(database); // required inside a migration to pass the wrapper
-
-        }*/
     }
 
 }
