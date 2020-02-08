dacb144f8a42616a5a34fc79cfeb96068319d2e7
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-02-08 11:57:51 +0800

    Settin up keys

--- app/src/main/java/org/openobservatory/ooniprobe/fragment/PreferenceFragment.java
+++ app/src/main/java/org/openobservatory/ooniprobe/fragment/PreferenceFragment.java
@@ -22,6 +22,10 @@ import org.openobservatory.ooniprobe.R;
 import org.openobservatory.ooniprobe.activity.MainActivity;
 import org.openobservatory.ooniprobe.activity.PreferenceActivity;
 import org.openobservatory.ooniprobe.common.Application;
+import org.openobservatory.ooniprobe.test.suite.InstantMessagingSuite;
+import org.openobservatory.ooniprobe.test.suite.MiddleBoxesSuite;
+import org.openobservatory.ooniprobe.test.suite.PerformanceSuite;
+import org.openobservatory.ooniprobe.test.suite.WebsitesSuite;
 
 import java.io.Serializable;
 
@@ -52,11 +56,17 @@ public class PreferenceFragment extends ExtendedPreferenceFragment<PreferenceFra
     public void onResume() {
         assert getArguments() != null;
         super.onResume();
-        if ("Websites".equals(rootKey)) {
+        if (getString(R.string.websites).equals(rootKey))
             setPreferencesFromResource(R.xml.preferences_websites, rootKey);
-        }
+        else if (getString(R.string.instant_messaging).equals(rootKey))
+            setPreferencesFromResource(R.xml.preferences_instant_messaging, rootKey);
+        else if (getString(R.string.middle_boxes).equals(rootKey))
+            setPreferencesFromResource(R.xml.preferences_middleboxes, rootKey);
+        else if (getString(R.string.performance).equals(rootKey))
+            setPreferencesFromResource(R.xml.preferences_performance, rootKey);
         else
             setPreferencesFromResource(getArguments().getInt(ARG_PREFERENCES_RES_ID), getArguments().getInt(ARG_CONTAINER_RES_ID), getArguments().getString(ARG_PREFERENCE_ROOT));
+
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
         getActivity().setTitle(getPreferenceScreen().getTitle());
         for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
