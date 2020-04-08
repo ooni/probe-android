6124aea0f91a630bbb9582765813ce403b0b5f34
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-04-08 09:01:30 +0200

    Duplicate classes

--- app/src/experimental/java/org/openobservatory/ooniprobe/common/FlavorApplication.java
+++
@@ -1,9 +0,0 @@
-package org.openobservatory.ooniprobe.common;
-
-import android.content.Context;
-
-class FlavorApplication {
-	public static void onCreate(Context context, boolean sendCrash) {
-		// does nothing
-	}
-}
