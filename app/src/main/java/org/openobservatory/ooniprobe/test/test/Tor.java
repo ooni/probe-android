0e9fae4e78cead6fe1900f977cc94bae7679861c
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-05-11 11:32:49 +0200

    Creating circumvention classes

---
+++ app/src/main/java/org/openobservatory/ooniprobe/test/test/Tor.java
@@ -0,0 +1,33 @@
+package org.openobservatory.ooniprobe.test.test;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.Gson;
+
+import org.openobservatory.ooniprobe.R;
+import org.openobservatory.ooniprobe.common.PreferenceManager;
+import org.openobservatory.ooniprobe.model.database.Measurement;
+import org.openobservatory.ooniprobe.model.database.Result;
+import org.openobservatory.ooniprobe.model.jsonresult.JsonResult;
+import org.openobservatory.ooniprobe.model.settings.Settings;
+
+public class Tor extends AbstractTest{
+    public static final String NAME = "tor";
+    private static final String MK_NAME = "Tor";
+
+    public Tor() {
+        super(NAME, MK_NAME, R.string.Test_Tor_Fullname, R.drawable.test_telegram, R.string.urlTestTor, 40);
+    }
+
+    @Override public void run(Context c, PreferenceManager pm, Gson gson, Result result, int index, AbstractTest.TestCallback testCallback) {
+        Settings settings = new Settings(c, pm);
+        run(c, pm, gson, settings, result, index, testCallback);
+    }
+
+    @Override public void onEntry(Context c, PreferenceManager pm, @NonNull JsonResult json, Measurement measurement) {
+        super.onEntry(c, pm, json, measurement);
+        //TODO
+    }
+}
