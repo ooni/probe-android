9aa46ec05fe5bd5ea994564a1cef6b9b2adcbbb7
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-05-18 10:22:44 +0200

    Test Result Details fixed

--- app/src/main/java/org/openobservatory/ooniprobe/test/test/Tor.java
+++ app/src/main/java/org/openobservatory/ooniprobe/test/test/Tor.java
@@ -13,12 +13,12 @@ import org.openobservatory.ooniprobe.model.database.Result;
 import org.openobservatory.ooniprobe.model.jsonresult.JsonResult;
 import org.openobservatory.ooniprobe.model.settings.Settings;
 
-public class Tor extends AbstractTest{
+public class Tor extends AbstractTest {
     public static final String NAME = "tor";
     private static final String MK_NAME = "Tor";
 
     public Tor() {
-        super(NAME, MK_NAME, R.string.Test_Tor_Fullname, R.drawable.test_telegram, R.string.urlTestTor, 40);
+        super(NAME, MK_NAME, R.string.Test_Tor_Fullname, R.drawable.test_tor, R.string.urlTestTor, 40);
     }
 
     @Override public void run(Context c, PreferenceManager pm, Gson gson, Result result, int index, AbstractTest.TestCallback testCallback) {
