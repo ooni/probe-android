41508c8e87ef83674514c7dd3f2596a28b80c1e4
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-06-25 13:17:49 +0200

    Implement countly exception report

--- app/src/main/java/org/openobservatory/ooniprobe/common/ExceptionManager.java
+++ app/src/main/java/org/openobservatory/ooniprobe/common/ExceptionManager.java
@@ -1,8 +1,13 @@
 package org.openobservatory.ooniprobe.common;
 
+import ly.count.android.sdk.Countly;
+
 public class ExceptionManager {
     public static void logException(Exception e){
         //TODO-COUNTLY migrate from Crashlytics to Countly
         Crashlytics.logException(e);
+        //TODO-COUNTLY which one? https://support.count.ly/hc/en-us/articles/360037754031-Android#logging-handled-exceptions
+        Countly.sharedInstance().crashes().recordHandledException(e);
+        Countly.sharedInstance().crashes().recordUnhandledException(e);
     }
 }
