2bb0de9db5a4c752caef5b13e30741d259a4b777
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-06-25 13:31:19 +0200

    Delete Crashlytics.java

--- app/src/full/java/org/openobservatory/ooniprobe/common/Crashlytics.java
+++
@@ -1,7 +0,0 @@
-package org.openobservatory.ooniprobe.common;
-
-public class Crashlytics {
-	public static void logException(Throwable throwable) {
-		com.crashlytics.android.Crashlytics.logException(throwable);
-	}
-}
