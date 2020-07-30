bada1d23af2f32b4201866ad4674d5884c665ea3
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-07-30 18:00:27 +0200

    Create ExampleInstrumentedTest.java

---
+++ app/src/androidTest/java/org/openobservatory/ooniprobe/ExampleInstrumentedTest.java
@@ -0,0 +1,41 @@
+package org.openobservatory.ooniprobe;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.openobservatory.ooniprobe.activity.MainActivity;
+
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(JUnit4.class)
+public class ExampleInstrumentedTest {
+    @ClassRule
+    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testTakeScreenshot() {
+        Screengrab.screenshot("before_button_click");
+
+        // Your custom onView...
+        onView(withId(R.id.run_all)).perform(click());
+
+        Screengrab.screenshot("after_button_click");
+    }
+}
+
