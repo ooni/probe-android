7eae13a8174098aa46d830a9cde319c7e58def4c
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-10-01 12:43:24 +0100

    requested changes

--- app/src/androidTest/java/org/openobservatory/ooniprobe/test/WebConnectivityTest.java
+++ app/src/androidTest/java/org/openobservatory/ooniprobe/test/WebConnectivityTest.java
@@ -17,21 +17,20 @@ import java.util.Collections;
 import java.util.concurrent.CountDownLatch;
 
 public class WebConnectivityTest extends AbstractTest {
-    private static final String MK_NAME = "WebConnectivity";
+    private static final String EXPERIMENT_NAME = "WebConnectivity";
     private static final String TAG = "integration-test";
     private static final String UNUSED_KEY = "UNUSED_KEY";
     private static final String CLIENT_URL = "https://ams-pg.ooni.org";
 
     @Test
     public void runWebConnectivity() {
-        final CountDownLatch signal = new CountDownLatch(1);
         boolean submitted = false;
         String report_id_1 = "r1";
         String report_id_2 = "r2";
         OONIMKTask task = null;
         Settings settings = new Settings(c, a.getPreferenceManager());
         Gson gson = a.getGson();
-        settings.name = MK_NAME;
+        settings.name = EXPERIMENT_NAME;
         settings.inputs = Collections.singletonList("http://mail.google.com");
         settings.options.max_runtime = 10;
         settings.annotations.origin = TAG;
@@ -41,7 +40,6 @@ public class WebConnectivityTest extends AbstractTest {
             task = Engine.startExperimentTask(settings.toExperimentSettings(gson, c));
         } catch (Exception exc) {
             Assert.fail();
-            signal.countDown();
         }
         while (!task.isDone()){
             try {
@@ -58,14 +56,12 @@ public class WebConnectivityTest extends AbstractTest {
                         report_id_2 = jr.report_id;
                         if (jr.test_keys.blocking == null) {
                             Assert.fail();
-                            signal.countDown();
                         }
                         break;
                     case "failure.report_create":
                     case "failure.measurement_submission":
                     case "failure.startup":
                         Assert.fail();
-                        signal.countDown();
                         break;
                     case "status.measurement_submission":
                         submitted = true;
@@ -79,6 +75,5 @@ public class WebConnectivityTest extends AbstractTest {
         }
         Assert.assertEquals(report_id_1, report_id_2);
         Assert.assertTrue(submitted);
-        signal.countDown();
     }
 }
