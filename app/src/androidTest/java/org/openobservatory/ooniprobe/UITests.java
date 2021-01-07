bd7775cfa64cf1da7d34496268d0dfd3193f13cc
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2021-01-07 11:31:46 +0000

    Check snackbar

--- app/src/androidTest/java/org/openobservatory/ooniprobe/UITests.java
+++ app/src/androidTest/java/org/openobservatory/ooniprobe/UITests.java
@@ -11,14 +11,18 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.openobservatory.ooniprobe.activity.MainActivity;
 
+import java.util.Random;
+
 import tools.fastlane.screengrab.locale.LocaleTestRule;
 
 import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressBack;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.StringContains.containsString;
@@ -35,7 +39,7 @@ public class UITests {
     public void before() {
     }
 
-    @Test
+    //@Test
     public void testCustomURL() {
         onView(withId(R.id.dashboard)).perform(click());
         onView(withId(R.id.recycler))
@@ -52,8 +56,6 @@ public class UITests {
         onView(withId(R.id.recyclerView))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
-
-        //TODO TestResults_Details_Websites_Reachable_Content_Paragraph
         onView(withId(R.id.desc)).check(matches(withText(containsString("http://ooni.io is accessible"))));
     }
 
@@ -61,10 +63,6 @@ public class UITests {
     @Test
     public void testSettings() {
         onView(withId(R.id.settings)).perform(click());
-        //onView(withId(R.id.privacy)).perform(click());
-        //onView(withText(activityRule.getActivity().getResources().getString(R.string.privacy))).perform(click());
-        //onData(PreferenceMatchers.withKey(activityRule.getActivity().getString(R.string.privacy))).perform(click());
-        //onData(PreferenceMatchers.withTitleText("Notifications")).perform(click());
         /*
         References
         https://stackoverflow.com/questions/45172505/testing-android-preferencefragment-with-espresso
@@ -74,13 +72,28 @@ public class UITests {
                 .perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(R.string.Settings_Privacy_Label)),
                         click()));
 
+        //TODO do this only if the switch is enabled
+        //complications https://stackoverflow.com/questions/51678563/how-to-access-recyclerview-viewholder-with-espresso
         onView(withId(androidx.preference.R.id.recycler_view))
                 .perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(R.string.Settings_Sharing_UploadResults)),
                         click()));
+        onView(isRoot()).perform(pressBack());
 
-        //complications https://stackoverflow.com/questions/51678563/how-to-access-recyclerview-viewholder-with-espresso
-        
-        //onView(withId(androidx.preference.R.id.recycler_view)).check(matches(isEnabled()));
+        onView(withId(R.id.dashboard)).perform(click());
+        onView(withId(R.id.recycler))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.run)).perform(click());
+
+        onView(withId(R.id.testResults)).perform(click());
+        onView(withId(R.id.recycler))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        Random random = new Random();
+        int randomNumber = random.nextInt(3);
+        onView(withId(R.id.recyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(randomNumber, click()));
+
+        onView(withId(com.google.android.material.R.id.snackbar_text))
+                .check(matches(withText(R.string.Snackbar_ResultsNotUploaded_Text)));
     }
 
 }
