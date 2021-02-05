7d179a6690868af258727a0a9e1eeca76b9c3fb7
Lorenzo Primiterra <lorenzo@openobservatory.org>
2021-02-05 14:04:55 +0000

    Adding Sentry for crash report (#413)
    * Adding Sentry gradle
    
    * Update AndroidManifest.xml
    
    * Implement and init sentry
    
    * Update build.gradle
    
    * Update ExceptionManager
    
    * Update AndroidManifest.xml

--- app/src/full/java/org/openobservatory/ooniprobe/common/ExceptionManager.java
+++ app/src/full/java/org/openobservatory/ooniprobe/common/ExceptionManager.java
@@ -3,8 +3,11 @@ package org.openobservatory.ooniprobe.common;
 
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
+import io.sentry.Sentry;
+
 public class ExceptionManager {
     public static void logException(Exception e){
         FirebaseCrashlytics.getInstance().recordException(e);
+        Sentry.captureException(e);
     }
 }
