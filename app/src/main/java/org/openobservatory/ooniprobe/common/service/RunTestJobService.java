d5473546763b277f87bd2a8551a1b27d6d4920ad
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2021-03-11 10:24:52 +0000

    Removing commented code and unused class

--- app/src/main/java/org/openobservatory/ooniprobe/common/service/RunTestJobService.java
+++ app/src/main/java/org/openobservatory/ooniprobe/common/service/RunTestJobService.java
@@ -11,6 +11,7 @@ import androidx.core.content.ContextCompat;
 
 import org.openobservatory.ooniprobe.R;
 import org.openobservatory.ooniprobe.common.Application;
+import org.openobservatory.ooniprobe.common.CountlyManager;
 import org.openobservatory.ooniprobe.common.NotificationService;
 import org.openobservatory.ooniprobe.common.PreferenceManager;
 import org.openobservatory.ooniprobe.test.suite.AbstractSuite;
@@ -31,58 +32,14 @@ public class RunTestJobService extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters params) {
-        //Intent service = new Intent(getApplicationContext(), RunTestService.class);
+        CountlyManager.recordEvent("AutomaticTest_started");
         Application app = ((Application)getApplicationContext());
-        Log.d(TAG, "is started");
-
-        //TEST1 only send notification every hour
-        /*
-        String currentTime = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
-        NotificationService.setChannel(getApplicationContext(), "RunTestService", app.getString(R.string.Settings_AutomatedTesting_Label), false, false, false);
-        NotificationService.sendNotification(getApplicationContext(), "RunTestService", "Should run test", "Time is "+currentTime);
-*/
-/*
-        //TEST2 call the API
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try  {
-                    Application app = ((Application)getApplicationContext());
-                    ServiceUtil.callCheckInAPI(app);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        thread.start();
-*/
         new JobTask(this, app).execute(params);
-
-        //TODO
-        //jobFinished(jobParams, needResheduleBoolean);
-        /*
-        //TODO-SERVICE-BK log stuff in countly
-        PreferenceManager pm = app.getPreferenceManager();
-        AbstractSuite websitesSuite = new WebsitesSuite();
-        AbstractTest webConnectivity = new WebConnectivity();
-        //webConnectivity.setMax_runtime(pm.getMaxRuntimeAutoTest());
-        websitesSuite.setTestList(webConnectivity);
-        ArrayList<AbstractSuite> testSuites = new ArrayList<>(Arrays.asList(websitesSuite));
-
-        Intent serviceIntent = new Intent(getApplicationContext(), RunTestService.class);
-        serviceIntent.putExtra("testSuites", testSuites);
-
-        ContextCompat.startForegroundService(this, serviceIntent);
-        //getApplicationContext().startService(serviceIntent);
-        //ServiceUtil.scheduleJob(getApplicationContext()); // reschedule the job
-         */
         return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
-        System.out.println("SyncService onStopJob");
-        //jobFinished(params, true);
         return true;
     }
 
@@ -97,15 +54,13 @@ public class RunTestJobService extends JobService {
 
         @Override
         protected JobParameters doInBackground(JobParameters... params) {
-            System.out.println("SyncService doInBackground");
             ServiceUtil.callCheckInAPI(app);
             return params[0];
         }
 
         @Override
         protected void onPostExecute(JobParameters jobParameters) {
-            System.out.println("SyncService onPostExecute");
-            /*
+            /*  //TODO
              * In cases where the background work fails (it could be because of an HTTP request that failed, for example),
              * you may want to retry before the next expected execution of the job.
              * That's when you pass true for jobFinished's needsReschedule.
--- app/src/main/java/org/openobservatory/ooniprobe/common/service/ServiceUtil.java
+++ app/src/main/java/org/openobservatory/ooniprobe/common/service/ServiceUtil.java
@@ -18,6 +18,7 @@ import org.openobservatory.engine.OONIURLInfo;
 import org.openobservatory.ooniprobe.BuildConfig;
 import org.openobservatory.ooniprobe.R;
 import org.openobservatory.ooniprobe.common.Application;
+import org.openobservatory.ooniprobe.common.CountlyManager;
 import org.openobservatory.ooniprobe.common.ExceptionManager;
 import org.openobservatory.ooniprobe.common.NotificationService;
 import org.openobservatory.ooniprobe.common.PreferenceManager;
@@ -35,8 +36,7 @@ public class ServiceUtil {
     private static final int id = 100;
 
     public static void scheduleJob(Context context) {
-        //TODO remove comments
-        System.out.println("SyncService scheduleJob");
+        CountlyManager.recordEvent("AutomaticTest_activated");
         Application app = ((Application)context.getApplicationContext());
         PreferenceManager pm = app.getPreferenceManager();
         ComponentName serviceComponent = new ComponentName(context, RunTestJobService.class);
@@ -46,43 +46,22 @@ public class ServiceUtil {
         int networkConstraint = pm.testWifiOnly() ? JobInfo.NETWORK_TYPE_UNMETERED : JobInfo.NETWORK_TYPE_ANY;
         builder.setRequiredNetworkType(networkConstraint);
         builder.setRequiresCharging(pm.testChargingOnly());
-        //TODO as soon as it gets activated it start a test, making the app unusable
-        //builder.setRequiresDeviceIdle(true);
 
         /*
         * Specify that this job should recur with the provided interval, not more than once per period.
         * You have no control over when within this interval
         * this job will be executed, only the guarantee that it will be executed at most once within this interval.
         */
-        //builder.setPeriodic(60 * 60 * 1000);
-        builder.setPeriodic(1000 * 60 * 15L);
+        builder.setPeriodic(60 * 60 * 1000);
         builder.setPersisted(true); //Job scheduled to work after reboot
-
-        //Settings to consider for the future
-        //This tells your job to not start unless the user is not using their device and they have not used it for some time.
-        //builder.setRequiresDeviceIdle(true); // device should be idle
-
-        //REMOVED SETTINGS DUE TO:
-        //java.lang.IllegalArgumentException: Can't call setOverrideDeadline() on a periodic job
-
-        //job will fire after 5 seconds when the network becomes available, or no later than 5 minutes
-        //Specify that this job should be delayed by the provided amount of time.
-        //builder.setMinimumLatency(5 * 1000); // wait at least
-        //the job will be executed anyway after 5 minutes waiting
-        //Set deadline which is the maximum scheduling latency.
-        //builder.setOverrideDeadline(5 * 60 * 1000); // maximum delay
-        /* Minimum flex for a periodic job, in milliseconds. */
-        //http://androidxref.com/8.0.0_r4/xref/frameworks/base/core/java/android/app/job/JobInfo.java#110
-
-        //builder.setBackoffCriteria()
-
+        
         //JobScheduler is specifically designed for inexact timing, so it can combine jobs from multiple apps, to try to reduce power consumption.
         JobScheduler jobScheduler = context.getSystemService(JobScheduler.class);
         jobScheduler.schedule(builder.build());
     }
 
     public static void stopJob(Context context) {
-        System.out.println("SyncService stopJob");
+        CountlyManager.recordEvent("AutomaticTest_deactivated");
         JobScheduler jobScheduler = context.getSystemService(JobScheduler.class);
         jobScheduler.cancel(id);
     }
@@ -100,15 +79,9 @@ public class ServiceUtil {
                     ReachabilityManager.isCharging(app),
 					app.getPreferenceManager().getEnabledCategoryArr().toArray(new String[0]));
 			OONICheckInResults results = session.checkIn(ooniContext, config);
-
-            System.out.println("SyncService callAPITest " + results.webConnectivity.urls.size());
-            //String currentTime = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
-            //NotificationService.setChannel(app, "RunTestService", app.getString(R.string.Settings_AutomatedTesting_Label), false, false, false);
-            //NotificationService.sendNotification(app, "RunTestService", "Should run test", "Time is "+currentTime + "Url size " + results.webConnectivity.urls.size());
             if (results.webConnectivity != null) {
                 ArrayList<String> inputs = new ArrayList<>();
                 for (OONIURLInfo url : results.webConnectivity.urls){
-                    //System.out.println("SyncService GOT URL: " + url.url);
                     inputs.add(Url.checkExistingUrl(url.url, url.category_code, url.country_code).url);
                 }
                 AbstractSuite suite = AbstractSuite.getSuite(app, "web_connectivity",
@@ -122,7 +95,6 @@ public class ServiceUtil {
         }
         catch (Exception e) {
             e.printStackTrace();
-            System.out.println("SyncService callAPITest e " + e);
             ExceptionManager.logException(e);
         }
     }
