815b65c51dbc530f83df35e43b02784bd2414701
FilipeA <filiperamosalves@gmail.com>
2021-05-19 17:31:12 +0100

    updated: ResubmitTask testing (WIP)

--- app/src/test/java/org/openobservatory/ooniprobe/RobolectricAbstractTest.java
+++ app/src/test/java/org/openobservatory/ooniprobe/RobolectricAbstractTest.java
@@ -1,6 +1,10 @@
 package org.openobservatory.ooniprobe;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Looper;
 
 import androidx.test.core.app.ApplicationProvider;
 
@@ -11,8 +15,11 @@ import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.openobservatory.ooniprobe.common.Application;
 import org.openobservatory.ooniprobe.factory.TestApplication;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+
+import static org.robolectric.Shadows.shadowOf;
 
 @Config(application = TestApplication.class)
 @RunWith(RobolectricTestRunner.class)
@@ -29,5 +36,22 @@ public abstract class RobolectricAbstractTest {
     @After
     public void tearDown() {
         FlowManager.destroy();
+    }
+
+    public <T extends Activity> T buildActivity(Class<T> activityClass, Intent intent) {
+        return Robolectric.buildActivity(activityClass, intent)
+                .create()
+                .get();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public void idleTaskUntilFinished(AsyncTask task) {
+        try {
+            while (task.getStatus() != AsyncTask.Status.FINISHED) {
+                shadowOf(Looper.getMainLooper()).idle();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
