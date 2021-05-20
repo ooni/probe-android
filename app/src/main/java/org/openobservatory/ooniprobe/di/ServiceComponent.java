d8718a8af3549ed57e88116a4d60b96f940a80f4
FilipeA <filiperamosalves@gmail.com>
2021-05-20 17:34:45 +0100

    added: TestAsyncTask unit testing

--- app/src/main/java/org/openobservatory/ooniprobe/di/ServiceComponent.java
+++ app/src/main/java/org/openobservatory/ooniprobe/di/ServiceComponent.java
@@ -1,5 +1,6 @@
 package org.openobservatory.ooniprobe.di;
 
+import org.openobservatory.ooniprobe.common.service.RunTestService;
 import org.openobservatory.ooniprobe.common.service.ServiceUtil;
 import org.openobservatory.ooniprobe.di.annotations.PerService;
 
@@ -9,4 +10,5 @@ import dagger.Subcomponent;
 @Subcomponent()
 public interface ServiceComponent {
     void inject(ServiceUtil.Dependencies deps);
+    void inject(RunTestService service);
 }
