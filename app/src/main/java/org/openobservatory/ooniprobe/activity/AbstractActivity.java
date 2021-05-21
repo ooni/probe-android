4646827b7b03d89db8d0fdc72c65e31fe715676b
FilipeA <filiperamosalves@gmail.com>
2021-05-21 12:04:44 +0100

    added: domain class for MainActivity notification logic && testing

--- app/src/main/java/org/openobservatory/ooniprobe/activity/AbstractActivity.java
+++ app/src/main/java/org/openobservatory/ooniprobe/activity/AbstractActivity.java
@@ -7,10 +7,25 @@ import com.google.gson.Gson;
 import org.openobservatory.ooniprobe.client.OONIAPIClient;
 import org.openobservatory.ooniprobe.common.Application;
 import org.openobservatory.ooniprobe.common.PreferenceManager;
+import org.openobservatory.ooniprobe.di.ActivityComponent;
+import org.openobservatory.ooniprobe.di.AppComponent;
 
 import okhttp3.OkHttpClient;
 
 public abstract class AbstractActivity extends AppCompatActivity {
+
+	public Application getApp() {
+		return ((Application) getApplication());
+	}
+
+	public AppComponent getComponent() {
+		return getApp().getComponent();
+	}
+
+	public ActivityComponent getActivityComponent() {
+		return getApp().getActivityComponent();
+	}
+
 	public PreferenceManager getPreferenceManager() {
 		return ((Application) getApplication()).getPreferenceManager();
 	}
