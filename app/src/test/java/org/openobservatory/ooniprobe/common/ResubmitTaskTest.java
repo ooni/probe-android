eaed96458bb34c1c95c1a50fc9867b60595d71bc
FilipeA <filiperamosalves@gmail.com>
2021-05-25 10:27:27 +0100

    updated: PR(#15) feedback

--- app/src/test/java/org/openobservatory/ooniprobe/common/ResubmitTaskTest.java
+++ app/src/test/java/org/openobservatory/ooniprobe/common/ResubmitTaskTest.java
@@ -18,6 +18,7 @@ import org.openobservatory.ooniprobe.test.EngineProvider;
 import org.openobservatory.ooniprobe.test.suite.WebsitesSuite;
 import org.openobservatory.ooniprobe.utils.DatabaseUtils;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
