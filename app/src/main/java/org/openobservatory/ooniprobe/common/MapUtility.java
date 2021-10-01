2b50afbfa860b7008bb7ec8ef2919e63ce05fabe
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2021-10-01 10:07:17 -0400

    Update MapUtility.java

--- app/src/main/java/org/openobservatory/ooniprobe/common/MapUtility.java
+++ app/src/main/java/org/openobservatory/ooniprobe/common/MapUtility.java
@@ -3,7 +3,7 @@ package org.openobservatory.ooniprobe.common;
 import java.util.HashMap;
 
 public class MapUtility {
-    //Class replacement for HashMap's function getOrDefault that doesn't work in android < N
+    //Class replacement for HashMap's function getOrDefault that doesn't work in android < N (Android 7.0 Nougat)
     public static Object getOrDefaultCompat(HashMap map, Object key, Object defaultValue) {
         return ((map.get(key) != null) ? map.get(key) : defaultValue);
     }
