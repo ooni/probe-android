f15302d84ae92d6d91252be76924505fec5ab0aa
Lorenzo Primiterra <lorenzo@openobservatory.org>
2021-12-14 12:36:23 -0300

    run InstantMessagingSuite and CircumventionSuite with autorun (#465)
    * run InstantMessagingSuite and CircumventionSuite with autorun
    
    * fix: use the array of suites
    
    Co-authored-by: Simone Basso <bassosimone@gmail.com>

--- app/src/main/java/org/openobservatory/ooniprobe/common/service/ServiceUtil.java
+++ app/src/main/java/org/openobservatory/ooniprobe/common/service/ServiceUtil.java
@@ -15,7 +15,12 @@ import org.openobservatory.ooniprobe.common.Application;
 import org.openobservatory.ooniprobe.common.PreferenceManager;
 import org.openobservatory.ooniprobe.common.ReachabilityManager;
 import org.openobservatory.ooniprobe.domain.GenerateAutoRunServiceSuite;
+import org.openobservatory.ooniprobe.test.TestAsyncTask;
 import org.openobservatory.ooniprobe.test.suite.AbstractSuite;
+import org.openobservatory.ooniprobe.test.suite.CircumventionSuite;
+import org.openobservatory.ooniprobe.test.suite.InstantMessagingSuite;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
@@ -85,10 +90,14 @@ public class ServiceUtil {
                );
 
         AbstractSuite suite = d.generateAutoRunServiceSuite.generate(config);
+        ArrayList<AbstractSuite> testSuites = new ArrayList<>();
+        testSuites.add(suite);
+        testSuites.add(new InstantMessagingSuite());
+        testSuites.add(new CircumventionSuite());
 
         if (suite != null) {
             Intent serviceIntent = new Intent(app, RunTestService.class);
-            serviceIntent.putExtra("testSuites", suite.asArray());
+            serviceIntent.putExtra("testSuites", testSuites);
             serviceIntent.putExtra("storeDB", false);
             ContextCompat.startForegroundService(app, serviceIntent);
         }
