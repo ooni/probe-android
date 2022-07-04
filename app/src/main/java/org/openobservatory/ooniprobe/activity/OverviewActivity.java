ce8d926ee6fc3f88e55d39b2ed580fb0309f9d4d
Norbel AMBANUMBEN <norbel@openobservatory.org>
2022-07-04 08:56:22 +0100

    Convey information about experimental test better (#525)

--- app/src/main/java/org/openobservatory/ooniprobe/activity/OverviewActivity.java
+++ app/src/main/java/org/openobservatory/ooniprobe/activity/OverviewActivity.java
@@ -61,6 +61,7 @@ public class OverviewActivity extends AbstractActivity {
 			String experimentalLinks =
 					"\n\n* [STUN Reachability](https://github.com/ooni/spec/blob/master/nettests/ts-025-stun-reachability.md)" +
 					"\n\n* [DNS Check](https://github.com/ooni/spec/blob/master/nettests/ts-028-dnscheck.md)" +
+					String.format("\n\n %s \n\n", getString(R.string.Dashboard_Experimental_Overview_Paragraph_AutomatedTesting)) +
 					"\n\n* [Tor Snowflake](https://ooni.org/nettest/tor-snowflake/)" +
 					"\n\n* [Vanilla Tor](https://github.com/ooni/spec/blob/master/nettests/ts-016-vanilla-tor.md)";
 			Markwon.setMarkdown(desc, getString(testSuite.getDesc1(), experimentalLinks));
