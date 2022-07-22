551284930c68d1602b60069673f1aecad5bbad5d
Norbel Ambanumben <aanorbel@gmail.com>
2022-07-22 10:19:09 +0100

    modified datasource for test restults

--- app/src/main/java/org/openobservatory/ooniprobe/domain/QueryDataSource.java
+++ app/src/main/java/org/openobservatory/ooniprobe/domain/QueryDataSource.java
@@ -5,13 +5,18 @@ import androidx.annotation.Nullable;
 import androidx.paging.PagingSource;
 import androidx.paging.PagingState;
 
+import com.google.common.collect.Iterables;
 import com.raizlabs.android.dbflow.sql.language.SQLOperator;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 
+import org.openobservatory.ooniprobe.common.ThirdPartyServices;
 import org.openobservatory.ooniprobe.model.database.Result;
 import org.openobservatory.ooniprobe.model.database.Result_Table;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import kotlin.coroutines.Continuation;
 
@@ -33,26 +38,31 @@ public class QueryDataSource extends PagingSource<Integer, Result> {
     public LoadResult.Page<Integer, Result> load(@NonNull LoadParams<Integer> loadParams, @NonNull Continuation<? super LoadResult<Integer, Result>> continuation) {
         // Key may be null during a refresh, if no explicit key is passed into Pager
         // construction. Use 0 as default, because our API is indexed started at index 0
-        int pageNumber = loadParams.getKey() != null ? loadParams.getKey() : 0;
+        int highestId = loadParams.getKey() != null ? loadParams.getKey() : 0;
 
-        SQLOperator[] conditions = (testGroupNameFilter != null && !testGroupNameFilter.isEmpty())
-                ? new SQLOperator[]{Result_Table.test_group_name.is(testGroupNameFilter)}
-                : new SQLOperator[0];
+        List<SQLOperator> conditions = (testGroupNameFilter != null && !testGroupNameFilter.isEmpty())
+                ? Collections.singletonList(Result_Table.test_group_name.is(testGroupNameFilter))
+                : new ArrayList<>();
+        if (highestId != 0) {
+            conditions.add(Result_Table.id.lessThan(highestId));
+        }
 
         List<Result> response = SQLite.select().from(Result.class)
-                .where(conditions)
+                .where(conditions.toArray(new SQLOperator[0]))
                 .limit(20)
-                .offset(pageNumber * 20)
-                .orderBy(Result_Table.start_time, false)
+                .orderBy(Result_Table.id, false)
                 .queryList();
 
-        // Since 0 is the lowest page number, return null to signify no more pages should
-        // be loaded before it.
-        Integer prevKey = (pageNumber > 0) ? pageNumber - 1 : null;
-
+        Integer prevKey;
+        try {
+            prevKey = (!response.isEmpty()) ? Objects.requireNonNull(Iterables.getFirst(response, null)).id - 20 : null;
+        } catch (Exception e) {
+            prevKey = null;
+            ThirdPartyServices.logException(e);
+        }
         // This API defines that it's out of data when a page returns empty. When out of
         // data, we return `null` to signify no more pages should be loaded
-        Integer nextKey = (!response.isEmpty()) ? pageNumber + 1 : null;
+        Integer nextKey = (!response.isEmpty()) ? Iterables.getLast(response).id : null;
         return new LoadResult.Page<>(
                 response,
                 prevKey,
