1412af108af3d02ec348904f700ca4346312d724
Norbel Ambanumben <aanorbel@gmail.com>
2023-10-26 16:51:17 +0100

    Convert `CustomWebsiteActivity` to kotlin and added `ConfirmDialogFragment`

---
+++ app/src/main/java/org/openobservatory/ooniprobe/activity/CustomWebsiteActivity.kt
@@ -0,0 +1,106 @@
+package org.openobservatory.ooniprobe.activity
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.os.Parcelable
+import android.util.Patterns
+import android.view.MenuItem
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.openobservatory.ooniprobe.fragment.ConfirmDialogFragment
+import org.openobservatory.ooniprobe.R
+import org.openobservatory.ooniprobe.adapters.CustomWebsiteRecyclerViewAdapter
+import org.openobservatory.ooniprobe.adapters.ItemRemovedListener
+import org.openobservatory.ooniprobe.common.PreferenceManager
+import org.openobservatory.ooniprobe.databinding.ActivityCustomwebsiteBinding
+import org.openobservatory.ooniprobe.model.database.Url
+import org.openobservatory.ooniprobe.test.suite.WebsitesSuite
+import java.io.Serializable
+import javax.inject.Inject
+
+class CustomWebsiteActivity : AbstractActivity(), ConfirmDialogFragment.OnClickListener {
+    @Inject
+    lateinit var preferenceManager: PreferenceManager
+    private lateinit var adapter: CustomWebsiteRecyclerViewAdapter
+    private lateinit var binding: ActivityCustomwebsiteBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityComponent.inject(this)
+        binding = ActivityCustomwebsiteBinding.inflate(
+            layoutInflater
+        )
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val layoutManager = LinearLayoutManager(this)
+        binding.urlContainer.layoutManager = layoutManager
+        adapter = CustomWebsiteRecyclerViewAdapter(object : ItemRemovedListener {
+            override fun onItemRemoved(position: Int) {
+                binding.bottomBar.title = getString(
+                    R.string.OONIRun_URLs, adapter.itemCount.toString()
+                )
+            }
+        })
+        binding.bottomBar.setOnMenuItemClickListener { item: MenuItem? ->
+            val items = adapter.getItems()
+            val urls = ArrayList<String>(items.size)
+            for (value in items) {
+                val sanitizedUrl = value.replace("\\r\\n|\\r|\\n".toRegex(), " ")
+                //https://support.microsoft.com/en-us/help/208427/maximum-url-length-is-2-083-characters-in-internet-explorer
+                if (Patterns.WEB_URL.matcher(sanitizedUrl)
+                        .matches() && sanitizedUrl.length < 2084
+                ) urls.add(
+                    Url.checkExistingUrl(sanitizedUrl).toString()
+                )
+            }
+            val suite = WebsitesSuite()
+            suite.getTestList(preferenceManager)[0].inputs = urls
+            RunningActivity.runAsForegroundService(
+                this@CustomWebsiteActivity, suite.asArray(), { finish() }, preferenceManager
+            )
+            true
+        }
+        binding.add.setOnClickListener { add() }
+
+        binding.urlContainer.adapter = adapter
+        add()
+    }
+
+    override fun onBackPressed() {
+        val base = getString(R.string.http)
+        val edited = adapter.itemCount > 0 && adapter.getItems()[0] != base
+        if (edited) {
+            ConfirmDialogFragment(
+                title = "Are you sure?",
+                message = "Your URLs will not be saved when you leave this screen. Are you sure you want to do that?",
+            ).show(supportFragmentManager, null)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    fun add() {
+        adapter.addAll(listOf(getString(R.string.http)))
+        binding.bottomBar.title = getString(
+            R.string.OONIRun_URLs, adapter.itemCount.toString()
+        )
+        adapter.notifyDataSetChanged()
+        scrollToBottom()
+    }
+
+    private fun scrollToBottom() {
+        binding.urlContainer.scrollToPosition(adapter.itemCount - 1)
+        binding.urlsList.post { binding.urlsList.fullScroll(View.FOCUS_DOWN) }
+    }
+
+    override fun onConfirmDialogClick(
+        serializable: Serializable?, parcelable: Parcelable?, buttonClicked: Int
+    ) {
+        if (buttonClicked == DialogInterface.BUTTON_POSITIVE) super.onBackPressed()
+    }
+}
---
+++ app/src/main/java/org/openobservatory/ooniprobe/fragment/ConfirmDialogFragment.kt
@@ -0,0 +1,104 @@
+package org.openobservatory.ooniprobe.fragment
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.os.Parcelable
+import androidx.annotation.IntDef
+import androidx.core.text.HtmlCompat
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.io.Serializable
+
+class ConfirmDialogFragment(
+    serializable: Serializable? = null,
+    parcelable: Parcelable? = null,
+    title: String? = null,
+    message: String? = null,
+    positiveButton: String? = null,
+    negativeButton: String? = null,
+    neutralButton: String? = null
+) : DialogFragment(), DialogInterface.OnClickListener {
+    companion object {
+        private const val MESSAGE = "MESSAGE"
+        private const val TITLE = "TITLE"
+        private const val POSITIVE_BUTTON = "POSITIVE_BUTTON"
+        private const val NEGATIVE_BUTTON = "NEGATIVE_BUTTON"
+        private const val NEUTRAL_BUTTON = "NEUTRAL_BUTTON"
+        private const val SERIALIZABLE = "SERIALIZABLE"
+        private const val PARCELABLE = "PARCELABLE"
+    }
+
+    private val listener: OnClickListener
+        get() = parentFragment as? OnClickListener ?: requireActivity() as OnClickListener
+
+    init {
+        Bundle().apply {
+            title?.let { putString(TITLE, it) }
+            message?.let { putString(MESSAGE, it) }
+            positiveButton?.let { putString(POSITIVE_BUTTON, it) }
+            negativeButton?.let { putString(NEGATIVE_BUTTON, it) }
+            neutralButton?.let { putString(NEUTRAL_BUTTON, it) }
+            serializable?.let { putSerializable(SERIALIZABLE, it) }
+            parcelable?.let { putParcelable(PARCELABLE, it) }
+        }.let {
+            if (!it.isEmpty) arguments = it
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        isCancelable = false
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?) =
+        MaterialAlertDialogBuilder(requireContext()).apply {
+            setTitle(requireArguments().getString(TITLE))
+            if (requireArguments().containsKey(MESSAGE)) setMessage(
+                HtmlCompat.fromHtml(
+                    requireArguments().getString(MESSAGE)!!,
+                    HtmlCompat.FROM_HTML_MODE_COMPACT
+                )
+            )
+            setPositiveButton(
+                requireArguments().getString(
+                    POSITIVE_BUTTON,
+                    getString(android.R.string.ok)
+                ), this@ConfirmDialogFragment
+            )
+            setNegativeButton(
+                requireArguments().getString(
+                    NEGATIVE_BUTTON,
+                    getString(android.R.string.cancel)
+                ), this@ConfirmDialogFragment
+            )
+            if (requireArguments().containsKey(NEUTRAL_BUTTON)) setNeutralButton(
+                requireArguments().getString(
+                    NEUTRAL_BUTTON
+                ), this@ConfirmDialogFragment
+            )
+        }.create()
+
+    override fun onClick(dialog: DialogInterface, which: Int) {
+        listener.onConfirmDialogClick(
+            requireArguments().getSerializable(SERIALIZABLE),
+            requireArguments().getParcelable(PARCELABLE),
+            which
+        )
+    }
+
+    interface OnClickListener {
+        fun onConfirmDialogClick(
+            serializable: Serializable?,
+            parcelable: Parcelable?,
+            @ConfirmDialogButton buttonClicked: Int
+        )
+    }
+}
+
+@Retention(AnnotationRetention.SOURCE)
+@IntDef(
+    DialogInterface.BUTTON_POSITIVE,
+    DialogInterface.BUTTON_NEGATIVE,
+    DialogInterface.BUTTON_NEUTRAL
+)
+internal annotation class ConfirmDialogButton
