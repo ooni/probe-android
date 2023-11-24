11bad2b32f188afbd3708eb05156ed0cb0f778a7
Norbel Ambanumben <aanorbel@gmail.com>
2023-11-24 18:53:29 +0100

    updated riseupvpn test

--- app/src/main/java/org/openobservatory/ooniprobe/test/test/RiseupVPN.java
+++ app/src/main/java/org/openobservatory/ooniprobe/test/test/RiseupVPN.java
@@ -17,11 +17,13 @@ import org.openobservatory.ooniprobe.model.database.Result;
 import org.openobservatory.ooniprobe.model.jsonresult.JsonResult;
 import org.openobservatory.ooniprobe.model.settings.Settings;
 
+@Deprecated
 public class RiseupVPN extends AbstractTest {
     public static final String NAME = "riseupvpn";
 
     public RiseupVPN() {
-        super(NAME, R.string.Test_RiseupVPN_Fullname, R.drawable.test_riseupvpn, R.string.urlTestRvpn, 15);
+        // NOTE: this test has been demoted to experimental and such the icon resource `R.drawable.test_riseupvpn` is not displayed anymore.
+        super(NAME, R.string.Test_RiseupVPN_Fullname, 0, R.string.urlTestRvpn, 15);
     }
 
     @Override public void run(Context c, PreferenceManager pm, AppLogger logger, Gson gson, Result result, int index, AbstractTest.TestCallback testCallback) {
