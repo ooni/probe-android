1faff56384dcbed35eaa813b31381c1f27a8f519
Norbel AMBANUMBEN <aanorbel@gmail.com>
2024-01-10 08:13:17 +0100

    Updated code based on review

--- scripts/build_experimental_archive.sh
+++ scripts/build_experimental_archive.sh
@@ -5,8 +5,12 @@ set -euxo pipefail
 gitTarget=${1:-"master"}
 buildDir=${2:-"./build"}
 
+if [ -d "$buildDir" ]; then rm -Rf $buildDir; fi
+
 # Create build directory and navigate into it
-mkdir -p "${buildDir}" && cd "${buildDir}"
+mkdir -p "${buildDir}"
+
+cd "${buildDir}"
 
 # Clone the repository
 git clone -v https://github.com/ooni/probe-cli.git
@@ -14,11 +18,8 @@ git clone -v https://github.com/ooni/probe-cli.git
 # Navigate into the repository
 cd probe-cli
 
-git checkout master
-
-git pull origin
 # Checkout the target branch
-git checkout -c "${gitTarget}"
+git checkout "${gitTarget}"
 
 # Build the probe-cli
 make android
