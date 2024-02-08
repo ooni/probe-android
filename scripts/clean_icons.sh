d2eb7b8d760b4f90ccf28ea8f8d3adab74c232c0
Norbel AMBANUMBEN <aanorbel@gmail.com>
2024-02-08 20:32:00 +0100

    Updated scripts

--- scripts/clean_icons.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Define workDir
-workDir=${1:-"./app/src/main/res/drawable"}
-
-# Delete all files contained in .gitignore
-while IFS= read -r file; do
-    rm -v "${workDir}/${file}"
-done < "${workDir}/.gitignore" || exit 0
