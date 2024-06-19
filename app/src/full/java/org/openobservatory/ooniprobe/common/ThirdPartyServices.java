e144b29ab9e4efbfd8b229c8af5532c30e7a37e1
Norbel AMBANUMBEN <aanorbel@gmail.com>
2024-06-19 15:44:28 +0100

    chore(deps): update `com.google.android.play:core` from `1.10.3` to `com.google.android.play:app-update:2.1.0`

--- app/src/full/java/org/openobservatory/ooniprobe/common/ThirdPartyServices.java
+++ app/src/full/java/org/openobservatory/ooniprobe/common/ThirdPartyServices.java
@@ -4,12 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.IntentSender;
 
+import com.google.android.gms.tasks.Task;
 import com.google.android.play.core.appupdate.AppUpdateInfo;
 import com.google.android.play.core.appupdate.AppUpdateManager;
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
 import com.google.android.play.core.install.model.AppUpdateType;
 import com.google.android.play.core.install.model.UpdateAvailability;
-import com.google.android.play.core.tasks.Task;
 import com.google.firebase.FirebaseApp;
 
 import org.jetbrains.annotations.NotNull;
