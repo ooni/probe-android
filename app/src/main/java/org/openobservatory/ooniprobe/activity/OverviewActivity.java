8c0cadfd2b58b89cf4fda52c193bb20059e9c375
Norbel AMBANUMBEN <aanorbel@gmail.com>
2024-08-28 14:03:50 +0100

    chore: add openvpn to experimental suite

--- app/src/main/java/org/openobservatory/ooniprobe/activity/OverviewActivity.java
+++ app/src/main/java/org/openobservatory/ooniprobe/activity/OverviewActivity.java
@@ -58,7 +58,7 @@ public class OverviewActivity extends AbstractActivity {
 		if (testSuite.getName().equals(ExperimentalSuite.NAME)) {
 			String experimentalLinks =
 					"\n\n* [STUN Reachability](https://github.com/ooni/spec/blob/master/nettests/ts-025-stun-reachability.md)" +
-					"\n\n* [DNS Check](https://github.com/ooni/spec/blob/master/nettests/ts-028-dnscheck.md)" +
+					"\n\n* [OpenVPN](https://github.com/ooni/spec/blob/master/nettests/ts-040-openvpn.md)" +
 					"\n\n* [RiseupVPN](https://ooni.org/nettest/riseupvpn/)" +
 					"\n\n* [ECH Check](https://github.com/ooni/spec/blob/master/nettests/ts-039-echcheck.md)" +
 					"\n\n* [Tor Snowflake](https://ooni.org/nettest/tor-snowflake/) "+ String.format(" ( %s )",getString(R.string.Settings_TestOptions_LongRunningTest))+
